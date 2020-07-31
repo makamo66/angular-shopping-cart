c0daa236dd70b8252274c9f57ebbff4c4a793579
makamo66 <makamo66@hotmail.com>
2020-07-31 16:41:01 -0400

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './shared/auth.service';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import * as firebase from 'firebase/app';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+
+export class AppComponent{
+
+}
---
+++ app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { environment } from './../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { AuthService } from './shared/auth.service';
+import { FormsModule, FormControl, ReactiveFormsModule, NgForm } from '@angular/forms';
+import { GrandTotalPipe } from './grandtotal.pipe';
+//import { ReactiveFormsComponent } from './reactive-forms.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+	ShoppingCartComponent,
+	//GrandTotalPipe,
+	//ReactiveFormsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+	AppRoutingModule,
+	ReactiveFormsModule,
+    FormsModule 
+  ],
+providers: [AuthService],
+  bootstrap: [AppComponent ]
+})
+export class AppModule { 
+
+}
