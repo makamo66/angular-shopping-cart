9f27f99fff42845ecb7675dda05aebbdbede7b57
makamo66 <makamo66@hotmail.com>
2020-08-09 14:36:00 -0400

    Add files via upload

--- shopping-cart.component.ts
+++ shopping-cart.component.ts
@@ -18,39 +18,62 @@ myForm: FormGroup;
 yourForm: FormGroup;
 
 items = [];
-//items  : any[];
+//items = Array();
+//items : string[] = new Array<string>();
 products: any[];
 quantity: number;
-
+//product_name: string;
 product_price: number;
+
+//totals : number[ ] = [ ];
 
 totals = [];
 
-isSubmitted : boolean;
+//isSubmitted : boolean;
 
+isSubmitted = false;
 storageKey = 'MyDataStorageKey';
 
-public onSubmit(thumbnail, quantity, product_name, product_price){
+public onSubmit(id, thumbnail, quantity, product_name, product_price){
 
 this.product_price = parseFloat(product_price);
 
-const data = {
+
+var data = {
+    id,
    thumbnail,
    quantity,
    product_name,
    product_price
 };
 
-this.items.push(data);
-localStorage.setItem(this.storageKey, JSON.stringify(this.items));
-this.isSubmitted = true;
-}
 
+this.isSubmitted = true;
+   //var retrieverObject = [];
+var retrieverObject = localStorage.getItem('items');
+    var retrieveObject = JSON.parse(retrieverObject);
+alert(retrieverObject);
+alert(id);
+  if (retrieverObject === null || retrieverObject === '[]' ) {
+    this.items.push(data);
+    localStorage.setItem(this.storageKey, JSON.stringify(this.items));
+  } else {
+    retrieveObject.forEach(el => {
+  if (el.id == id && retrieverObject !== '[]' ){
+        this.quantity += quantity;
+      } else {
+        this.items.push(data);
+        localStorage.setItem(this.storageKey, JSON.stringify(this.items));
+      }
+    })    
+ }
+}
 get grandTotal() {
 
 let i;
 let sub_total = 0;
 let grand_total = 0;
+//this.totals = [];
 ///if  (this.isSubmitted() == true) {
 if  (this.isSubmitted == true) {
  if (typeof this.product_price  !== "undefined" && typeof this.quantity  !== "undefined") {
@@ -63,6 +86,7 @@ if  (this.isSubmitted == true) {
                 for (i = 0; i < this.totals.length; i++) {
                         grand_total += this.totals[i];
                 }
+		//this.isSubmitted = false;
         return grand_total;
 }
 
@@ -91,12 +115,17 @@ this.quantity=product.nullValue;
 }
 
 deleteItem(i){
-
+this.totals = [];
+this.isSubmitted = true;
+this.grandTotal;
+//this.grandTotal = this.grandTotal();
 this.items.splice(i,1);
-
+//this.items.slice(i,1);
+//this.items = this.items.filter((_, j) => j !== i);
 this.setStorageItems(this.items);
 
-console.log("took away condition");
+
+console.log("forum code");
 }
 
 
@@ -115,7 +144,8 @@ console.log("took away condition");
   }
 getStorageItems(): any[] {
     try {
-        return JSON.parse(localStorage.getItem('items'));
+	return JSON.parse(localStorage.getItem('MyDataStorageKey'))
+       // return JSON.parse(localStorage.getItem('items'));
     } catch(err) {
         console.warn(err);
         return [];
@@ -131,10 +161,11 @@ ngOnInit(): void {
     });
    this.yourForm = new FormGroup({
        //int: new FormControl()
-    });
-	this.items.length = 0;
-   this.items = this.getStorageItems();
- }
+    });	
+   
+ this.items = this.getStorageItems();
+  
+  }
  
    ngAfterContentChecked() {
     this.cd.detectChanges();
